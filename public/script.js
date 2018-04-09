82b749d95b061af08278ffd11e9faabeb54cf860
Samuel Terra <samuelterra22@hotmail.com>
2018-04-09 12:08:41 -0300

    Implementa logica de adicionar/remover no carrinho

--- public/script.js
+++ public/script.js
@@ -1,18 +1,60 @@
+var PRICE = 9.99
+
 new Vue({
   el: '#app',
-  data:{
+  data: {
     total: 0,
-    items:[
-      {title: 'Item 1'},
-      {title: 'Item 2'},
-      {title: 'Item 3'}
+    items: [
+      {id: 1, title: 'Item 1'},
+      {id: 2, title: 'Item 2'},
+      {id: 3, title: 'Item 3'}
     ],
-    cart:[]
+    cart: []
   },
-  methods:{
-    addItem:function (index) {
-      this.total += 9.99
-      this.cart.push(this.items[index])
+  methods: {
+    onSubmit: function () {
+      console.log('onSubmit')
+    },
+    addItem: function (index) {
+      this.total += PRICE
+      var item = this.items[index]
+      var found = false
+      for (var i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          found = true
+          this.cart[i].qty++
+          break
+        }
+      }
+      if (!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          qty: 1,
+          price: PRICE
+        })
+      }
+    },
+    inc: function (item) {
+      item.qty++
+      this.total += PRICE
+    },
+    dec: function (item) {
+      item.qty--
+      this.total -= PRICE
+      if (item.qty <= 0) {
+        for (var i = 0; i < this.cart.length; i++) {
+          if (this.cart[i].id === item.id) {
+            this.cart.splice(i, 1)
+            break
+          }
+        }
+      }
+    }
+  },
+  filters: {
+    currency: function (price) {
+      return '$'.concat(price.toFixed(2))
     }
   }
-});
+})
