e1558b4cd3234547caca90489f081a4bb02d0432
Samuel Terra <samuelterra22@hotmail.com>
2018-04-11 14:46:57 -0300

    Verificação de pesquisa vazia adicionada

--- public/script.js
+++ public/script.js
@@ -19,7 +19,6 @@ new Vue({
     }
   },
   methods: {
-
     appendItems: function () {
       if (this.items.length < this.results.length) {
         var append = this.results.slice(this.items.length, this.items.length + LOAD_NUM)
@@ -27,16 +26,18 @@ new Vue({
       }
     },
     onSubmit: function () {
-      this.items = []
-      this.loading = true
-      this.$http
-        .get('/search/'.concat(this.newSearch))
-        .then(function (res) {
-          this.lastSearch = this.newSearch
-          this.results = res.data
-          this.appendItems()
-          this.loading = false
-        })
+      if (this.newSearch.length) {
+        this.items = []
+        this.loading = true
+        this.$http
+          .get('/search/'.concat(this.newSearch))
+          .then(function (res) {
+            this.lastSearch = this.newSearch
+            this.results = res.data
+            this.appendItems()
+            this.loading = false
+          })
+      }
     },
     addItem: function (index) {
       this.total += PRICE
